17301626216bb4d45f2c549956cb43191ce02c4e
HyeonWooJo <HyeonWooJo@github.com>
2022-09-06 12:57:47 +0900

    Feat: 게시판 상세 포스팅, 수정 API unit test 구현

--- thingsflow/postings/tests.py
+++ thingsflow/postings/tests.py
@@ -1,3 +1,4 @@
+from email.quoprimime import body_check
 import json
 
 from django.test import TestCase, Client
@@ -44,16 +45,33 @@ class PostingDetailView(TestCase):
     def tearDown(self):
         Posting.objects.all().delete()
 
-    # 자유게시판 리스트 조회 API unit test
-    def test_success_list_view_get(self):
+    def test_success_detail_view_post(self):
         client   = Client()
-        response = client.get(
-            "/postings?offset=0&limit=1", 
+        body     = {
+            'title'   : 'Breaking Bad',
+            'context' : 'Best American Drama',
+            'psword'  : '2asdfg',
+        }
+        response = client.post(
+            "/postings/detail",
+            data=json.dumps(body),
             content_type='application/json')
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {'results' : [{
-            'id'      : 1,
-            'title'   : 'I am Saul good man',
-            'context' : 'It is 프리퀄 '
-        }]})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {'message' : 'SUCCESS'})
+
+    def test_success_detail_view_update(self):
+        client   = Client()
+        body     = {
+            'title'   : 'Breaking Bad',
+            'context' : 'Best American Drama',
+            'psword'  : '1asdfg',
+            'posting_id': 1
+        }
+        response = client.post(
+            "/postings/detail",
+            data=json.dumps(body),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {'message': 'UPDATED'})
