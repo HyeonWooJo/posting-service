f77bd601aef47ef4c7609cc61babd64550d1af83
HyeonWooJo <HyeonWooJo@github.com>
2022-09-06 12:50:32 +0900

    Feat: 게시판 리스트 API unit test

--- thingsflow/postings/tests.py
+++ thingsflow/postings/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from postings.models import Posting
+
+
+class PostingListView(TestCase):
+    def setUp(self):
+        Posting.objects.create(
+            id      = 1,
+            title   = 'I am Saul good man',
+            context = 'It is 프리퀄 of Breaking Bad',
+            psword  = '1asdfg'
+        )
+
+    def tearDown(self):
+        Posting.objects.all().delete()
+
+    # 자유게시판 리스트 조회 API unit test
+    def test_success_list_view_get(self):
+        client   = Client()
+        response = client.get(
+            "/postings?offset=0&limit=1", 
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'results' : [{
+            'id'      : 1,
+            'title'   : 'I am Saul good man',
+            'context' : 'It is 프리퀄 '
+        }]})
+
+
+class PostingDetailView(TestCase):
+    def setUp(self):
+        Posting.objects.create(
+            id      = 1,
+            title   = 'I am Saul good man',
+            context = 'It is 프리퀄 of Breaking Bad',
+            psword  = '1asdfg'
+        )
+
+    def tearDown(self):
+        Posting.objects.all().delete()
+
+    # 자유게시판 리스트 조회 API unit test
+    def test_success_list_view_get(self):
+        client   = Client()
+        response = client.get(
+            "/postings?offset=0&limit=1", 
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'results' : [{
+            'id'      : 1,
+            'title'   : 'I am Saul good man',
+            'context' : 'It is 프리퀄 '
+        }]})
